a95deab1d17a420b3cddd2ecc33570fad1f6a89d
Leader3545 <hlansicheinn@gmail.com>
2025-04-07 03:04:29 +0700

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+    AOS.init({
+      duration: 1000,
+      once: true
+    });
+  });
+  
+  function loadMoreNews() {
+    alert("ฟังก์ชันโหลดข่าวเพิ่มเติมอยู่ระหว่างการพัฒนา");
+  };  
